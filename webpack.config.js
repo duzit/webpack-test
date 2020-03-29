810eaa44489f885368962629bc261c50b58c215b
duzit <duduwei_oneday@163.com>
2020-03-29 11:02:26 +0800

    0329

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,21 @@
 
 const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
   entry: './demo.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+    // filename: 'bundle.js'
+    filename: 'js/[name]-[chunkhash].js'
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: 'index.html',
+      filename: 'index-[hash].html',
+      title: 'webpack test'
+    })
+  ]
 
 }
